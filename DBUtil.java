8b81116e263f72a1322adf060305dd557d779e98
rohinigavand9 <gavandrohini2@gmail.com>
2025-06-22 11:37:52 +0000

    First commit

---
+++ DBUtil.java
@@ -0,0 +1,8 @@
+Hello Guys,
+
+install git using = sudo yum install git -y (command)
+create new directory = mkdir axisbank
+stay in newly created directory = cd axisbank
+print working directory = pwd
+Create git empty local repository = git init
+Git version =   git --version

735756ca26dc05dd3492f4e844722d0ef0565730
rohini9104 <gavandrohini2@gmail.com>
2025-07-04 03:03:10 +0000

    deployed file

---
+++ Gitstepsnotes.sh
@@ -0,0 +1,11 @@
+Hello,
+Steps which follows from creating code/file to pushing into remote repository
+1. create one file using any editor ex . vi gitinfo.sh
+2. git status == Shows file present into which stage working or staging - if it in red color it means it present in 
+    working area.
+3. git add . == move file from working area to stating area
+4. git status == if file is in green color it means present in staging area
+5. git commit -m "any message" - It will commit the code , the message will apear on git page .
+6. git push ab master = It will push the code into remote repository and ask for username and password--pass will be PAT    personal access tocken
+7. Refresh git page - this file we can see on page/ repository.
+

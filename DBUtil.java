fdf3e69c4ad8ae9a4b8b3ab4399af500bf842ce4
rohini9104 <gavandrohini2@gmail.com>
2025-07-04 03:06:03 +0000

    updated file

--- DBUtil.java
+++ DBUtil.java
@@ -1,5 +1,5 @@
 Hello Guys,
-Basic steps in git 	
+Basic steps in git 	f
 install git using -- sudo yum install git -y= (sudo because it has root user previlages) only root user can install softwares
   created new directory -- mkdir axisbank
   cd axisbank ---enter in that directory

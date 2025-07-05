8c5d726f9e5d87a5903c175098f203f803612930
rohini9104 <gavandrohini2@gmail.com>
2025-07-05 06:03:57 +0000

    new file added

---
+++ test.py
@@ -0,0 +1,7 @@
+Hello ,
+How to move files into local repository?
+
+--> create file --that will present in working area
+    git add . -- it will move into staging area fro wa
+    git commit -m "msg"- after commiting it will move into local repository
+

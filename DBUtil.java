eaa294e07c612bda7a6ad2041081652b2fbf0ce8
rohini9104 <gavandrohini2@gmail.com>
2025-07-05 07:06:58 +0000

    Revert "latest update"
    This reverts commit 151dfabdf2f53c01e1fa5115e010b78e9aece15c.

--- DBUtil.java
+++ DBUtil.java
@@ -1,9 +1,9 @@
 Hello Guys,
-Basic steps in git	f
+Basic steps in git 	f
 install git using -- sudo yum install git -y= (sudo because it has root user previlages) only root user can install softwares
   created new directory -- mkdir axisbank
   cd axisbank ---enter in that directory
   Created empty local repository -- git init
   once created repository default branch name "master" will be created 
-  
+  Thank you.
 

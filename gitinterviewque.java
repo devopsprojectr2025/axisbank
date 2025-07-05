c5dfb2f87c98638d2a130cdba3e81dc6422802e4
rohini9104 <gavandrohini2@gmail.com>
2025-07-05 03:30:14 +0000

    Interview questiona

---
+++ gitinterviewque.java
@@ -0,0 +1,14 @@
+Hello,
+
+1. What is git?
+2. Wahat is repository in git ?
+3. Difference between git and github?
+4. Difference between working and staging area?
+5. What is commit in git?
+6. What is branch name in git and why it is used?
+7. why we create empty local repository?
+8. Difference between local and remote repository and how we create it?
+9. what is tracked and untracked files?
+10. How to add file into staging area from working area?
+11. what is organisation in git?
+12. what is PAT - personal access tocken in git?
